eb70be6ffc49a7fe29b4b7d2fe1b5172d2250ae8
maahi7991 <31163586+maahi7991@users.noreply.github.com>
2024-06-09 12:54:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file can contain any JavaScript needed for interaction
+// Currently, there's no interactive functionality to be added
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Mahesh Khetri website is loaded');
+});
